021694ee13085afc1bb3cae29aa53d83a28dd284
anhnt <anhnguyen1494@gmail.com>
2019-10-08 10:49:42 +0700

    add Google Analytics

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -71,6 +71,15 @@
         })(window,document,'script','dataLayer','GTM-WZ44WP5');</script>
 
     <!-- End Google Tag Manager -->
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-149601873-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-149601873-1');
+    </script>
     <!-- Yandex.Metrika counter -->
 
     <script type="text/javascript" >
