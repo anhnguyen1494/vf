ed681be1771432c7c5739f3b8230ce4ea01f82b3
anhnt <anhnguyen1494@gmail.com>
2019-10-10 15:36:05 +0700

    update script gg ads

--- resources/views/frontend/layout.blade.php
+++ resources/views/frontend/layout.blade.php
@@ -80,6 +80,15 @@
 
         gtag('config', 'UA-149601873-1');
     </script>
+    <!-- Global site tag (gtag.js) - Google Ads: 705046198 -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=AW-705046198"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'AW-705046198');
+    </script>
     <!-- Yandex.Metrika counter -->
 
     <script type="text/javascript" >
